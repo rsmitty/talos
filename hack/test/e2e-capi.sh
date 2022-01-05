6385bdd962c183d801b524b4351551292eaca6e3
Spencer Smith <spencer.smith@talos-systems.com>
2022-01-05 10:49:27 -0500

    test: update CAPI components to latest
    This PR updates to our latest point releases for CABPT and CACPPT to
    ensure faster testing. It also pulls in a newer AWS provider.
    
    Signed-off-by: Spencer Smith <spencer.smith@talos-systems.com>

--- hack/test/e2e-capi.sh
+++ hack/test/e2e-capi.sh
@@ -5,9 +5,9 @@ set -eou pipefail
 source ./hack/test/e2e.sh
 
 export CAPI_VERSION="${CAPI_VERSION:-1.0.2}"
-export CABPT_VERSION="${CABPT_VERSION:-0.5.0}"
-export CACPPT_VERSION="${CACPPT_VERSION:-0.4.0}"
-export CAPA_VERSION="${CAPA_VERSION:-1.1.0}"
+export CABPT_VERSION="${CABPT_VERSION:-0.5.1}"
+export CACPPT_VERSION="${CACPPT_VERSION:-0.4.2}"
+export CAPA_VERSION="${CAPA_VERSION:-1.2.0}"
 export CAPG_VERSION="${CAPG_VERSION:-1.0.0}"
 
 # We need to override this here since e2e.sh will set it to ${TMP}/capi/kubeconfig.
